335efefc06fce2ebf61bb1d15dfd123d2ef23202
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-02-01 09:31:23 +0100

    Add files via upload

---
+++ Klasa_Directory/Klasa_Directory/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Klasa_Directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite putanju direktorija");
+            string putanja = Console.ReadLine();
+
+            if(Directory.Exists(putanja))
+            {
+                Console.WriteLine("Datoteke:\n");
+                foreach (string datoteka in Directory.GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+            }
+
+            string putanjaPD = "\\test";
+            string novaPutanja = putanja + putanjaPD;
+
+            if (!Directory.Exists(novaPutanja)) Directory.CreateDirectory(novaPutanja);
+            else Console.WriteLine("Direktorij vec postoji");
+
+            Console.WriteLine("Poddirektorij:\n");
+            foreach(string podDirektorij in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(podDirektorij);
+            }
+
+            Directory.Delete(novaPutanja);
+
+            Console.WriteLine("Nakon brisanja: ");
+            foreach (string podDirektorij in Directory.GetDirectories(putanja))
+            {
+                Console.WriteLine(podDirektorij);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
